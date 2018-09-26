cfae9593e3b2b69f54fa575f5402028868bb4ff2
Emilia <emilia.smolko@pl.ibm.com>
2018-09-26 14:07:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='hello-world-python',
     version='1.0.0',
-    description='Pierwsza strona z wykorzystaniem Python Flask',
+    description='First page with Python Flask',
     long_description=long_description,
     url='https://github.com/emiliasmolko/HelloWorldPython',
 )
